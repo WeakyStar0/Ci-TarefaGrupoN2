9307be57e364c2bd4f2837e4a43880d35f56c779
Tiriri420 <tomas2002mlp@gmail.com>
2025-06-06 20:42:50 +0100

    Alguma Navegação

--- Assets/Scripts/LoadScenes.cs
+++ Assets/Scripts/LoadScenes.cs
@@ -1,15 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadScenes : MonoBehaviour
 {
-    // The name of the scene to load
-    [SerializeField]
-    private string sceneToLoad = "MainMenu";
-
-    
-    private void Start()
+    // Método público para carregar uma cena pelo nome (para ligar ao botão)
+    public void LoadSceneByName(string sceneName)
     {
-        // Load the main menu scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Método para carregar uma cena pelo índice (opcional)
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    // Método para voltar à cena anterior
+    public void GoBack()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex > 0)
+        {
+            SceneManager.LoadScene(currentSceneIndex - 1);
+        }
+        else
+        {
+            Debug.Log("Já estás na primeira cena, não dá para voltar mais.");
+        }
+    }
+
+    // Método para reiniciar a cena atual
+    public void ReloadCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    // Método para fechar a aplicação (funciona só no build)
+    public void QuitApp()
+    {
+        Application.Quit();
+        Debug.Log("Aplicação fechada.");
     }
 }
